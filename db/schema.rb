49ef0d43ad15d187fb4a9e36ac70eccf46ee00ce
DeadKennedyx <juan.dw.ft@gmail.com>
2015-07-09 11:08:40 -0500

    feature/gmail-login

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150703010934) do
+ActiveRecord::Schema.define(version: 20150709160620) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -74,5 +74,16 @@ ActiveRecord::Schema.define(version: 20150703010934) do
   end
 
   add_index "total_amounts", ["employee_id"], name: "index_total_amounts_on_employee_id", using: :btree
+
+  create_table "users", force: :cascade do |t|
+    t.string   "provider"
+    t.string   "uid"
+    t.string   "name"
+    t.string   "oauth_token"
+    t.datetime "oauth_expires_at"
+    t.datetime "created_at",                       null: false
+    t.datetime "updated_at",                       null: false
+    t.boolean  "is_admin",         default: false, null: false
+  end
 
 end
