d26dfdcf66776c18de92217cc16a2fec99cc45a1
DarkAbsolutE <dark_necros_absolute@hotmail.com>
2015-07-20 15:16:16 -0500

    Adding information for the salary and savings, initialize database, 0.0, 0.0, 0.0

--- lib/bamboohr/sync.rb
+++ lib/bamboohr/sync.rb
@@ -2,8 +2,13 @@ module BambooHR
   class Sync
     def self.employees
       employees = client.employee_list
-      employees['employees'].each do |data|
+      id = 1
+      employees['employees'].each do |data|	
         create_employee(data)
+	create_salary(id)
+	create_saving_employee(id)
+	create_saving_company(id)
+	id = id + 1
       end
     end
 
@@ -13,10 +18,23 @@ module BambooHR
         lastname: data['lastName'],
 	status: true
         }
-      if importable?(data)
+      if importable?(data)	
         employee = Employee.create_with(user_data).find_or_initialize_by(email: data['workEmail'])
         print '.' if  employee.save(validate: false)
       end
+    end
+
+    def self.create_salary(id)
+	salary = Salary.create_with(amount: 0).find_or_initialize_by(employee_id: id)
+	print '.' if  salary.save(validate: false)	
+    end
+
+    def self.create_saving_employee(id)
+	#saving_employee =
+    end
+
+    def self.create_saving_company(id)
+	#saving_campany =
     end
 
     def self.client
