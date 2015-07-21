ca4f3567922aa5fe62ba5e02e4ec7b997a0e8466
DeadKennedyx <juan.dw.ft@gmail.com>
2015-07-21 13:45:56 -0500

    fixed identation

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,9 +1,7 @@
 class HomesController < ApplicationController
   before_action :check_session, :only => [:show]
 
-
   def login
-
     if session[:employee_id] != nil
       redirect_to home_path
     else
