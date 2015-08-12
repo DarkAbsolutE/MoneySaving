72bba4d87748a0e3596fe8035c6cc4c649e45998
DarkAbsolutE <dark_necros_absolute@hotmail.com>
2015-08-12 17:30:56 -0500

    adding initialize code for salary

--- app/models/salary.rb
+++ app/models/salary.rb
@@ -1,3 +1,22 @@
 class Salary < ActiveRecord::Base
-    belongs_to :employee  
+    belongs_to :employee
+    salaries
+    create_salary
+
+    private
+      def self.salaries
+        employee = Employee.all
+        employee.each do |data|
+          create_salary(data)
+        end
+      end
+
+      def self.create_salary(data)
+        amount_data = {
+          employee_id: data['id'],
+          amount: 0.0
+        }
+        salary = Salary.create_with(amount_data).find_or_initialize_by(employee: data['id'])
+        salary.save
+      end
 end
