04ccce1cdad16357f0ee9c04f46f82d13963d8af
Imjustaguy <aldompe95@gmail.com>
2015-08-12 16:57:38 -0500

    fix identation

--- spec/features/changestatus_spec.rb
+++ spec/features/changestatus_spec.rb
@@ -1,10 +1,10 @@
 require 'spec_helper'
-  feature 'changestatus' do
-    background do
-      FactoryGirl.create(:admin, email: 'john@crowdint.com', name: 'Jonh Doe', is_admin: true, id: 1)
-      visit root_path
-      click_link("Sign in with your crowdint account")
-      expect(page).to have_content "Signed in as Jonh Doe!"
+feature 'changestatus' do
+  background do
+    FactoryGirl.create(:admin, email: 'john@crowdint.com', name: 'Jonh Doe', is_admin: true, id: 1)
+    visit root_path
+    click_link("Sign in with your crowdint account")
+    expect(page).to have_content "Signed in as Jonh Doe!"
   end
 
   scenario "User should be able to change status" do
