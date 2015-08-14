1317eec046d946c7f8ae2094bec410eece82971d
Imjustaguy <aldompe95@gmail.com>
2015-08-13 20:24:38 -0500

    add login method

--- spec/features/changestatus_spec.rb
+++ spec/features/changestatus_spec.rb
@@ -7,8 +7,6 @@ feature 'changestatus' do
 
   scenario "User should be able to change status" do
     click_link("Employees")
-    save_and_open_page
-    binding.pry
     expect(page).to have_content "Active Employees"
     click_link ""['btn ac']
     expect(page).to have_content "Inactive Employees"
