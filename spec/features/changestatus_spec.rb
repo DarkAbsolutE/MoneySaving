18b961bad8eada04f642d098debeed4bbdfefbd7
Imjustaguy <aldompe95@gmail.com>
2015-08-13 20:22:49 -0500

    add login method

--- spec/features/changestatus_spec.rb
+++ spec/features/changestatus_spec.rb
@@ -1,14 +1,14 @@
 require 'spec_helper'
+
 feature 'changestatus' do
   background do
-    FactoryGirl.create(:admin, email: 'john@crowdint.com', name: 'Jonh Doe', is_admin: true, id: 1)
-    visit root_path
-    click_link("Sign in with your crowdint account")
-    expect(page).to have_content "Signed in as Jonh Doe!"
+    login
   end
 
   scenario "User should be able to change status" do
     click_link("Employees")
+    save_and_open_page
+    binding.pry
     expect(page).to have_content "Active Employees"
     click_link ""['btn ac']
     expect(page).to have_content "Inactive Employees"
