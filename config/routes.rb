5e61637c6883c58840b750c324e730807d57edc8
Victor Velazquez <victor.velazquez@crowdint.com>
2015-08-18 18:50:22 -0500

    Change indentation routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
-    put 'update_status/:id', to: 'employees#update_status', as:'update_status'
-    get 'auth/:provider/callback', to: 'sessions#create'
-    get 'auth/failure', to: redirect('/')
-    get 'signout', to: 'sessions#destroy', as: 'signout'
-    resources :employees, except: [:new, :create, :destroy]
-    resources :sessions, only: [:create, :destroy]
-    resource :home, only: [:show]
-    resources :salaries, only: [:update]
-    root to: "homes#login"
+  put 'update_status/:id', to: 'employees#update_status', as:'update_status'
+  get 'auth/:provider/callback', to: 'sessions#create'
+  get 'auth/failure', to: redirect('/')
+  get 'signout', to: 'sessions#destroy', as: 'signout'
+  resources :employees, except: [:new, :create, :destroy]
+  resources :sessions, only: [:create, :destroy]
+  resource :home, only: [:show]
+  resources :salaries, only: [:update]
+  root to: "homes#login"
 end
