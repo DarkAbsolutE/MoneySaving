e704cb362e04af0c573061128dc9a6b40e38a8b7
DarkAbsolutE <dark_necros_absolute@hotmail.com>
2015-08-19 13:18:56 -0500

    Create initialize file for salaries, saving companies and employees

---
+++ lib/initialize/initialize_saving_employees.rb
@@ -0,0 +1,19 @@
+module Initialize
+  class Initialize_saving_employees
+    def self.saving_employees
+      employee = Employee.all
+      employee.each do |data|
+        create_saving_employee(data)
+      end
+    end
+
+    def self.create_saving_employee(data)
+      amount_data = {
+        employee_id: data['id'],
+        amount: 0.0
+      }
+      saaving_employee = SavingEmployee.create_with(amount_data).find_or_initialize_by(employee: data)
+      print '.' if saving_employee.save(validate: false)
+    end
+  end
+end
