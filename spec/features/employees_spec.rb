d78a481cc37614f35e708c93f1b7db7aef0da29b
Victor Velazquez <victor.velazquez@crowdint.com>
2015-08-21 03:37:45 -0500

    Remove wron testing scenarios

---
+++ spec/features/employees_spec.rb
@@ -0,0 +1,24 @@
+require 'spec_helper'
+
+feature 'Employees' do
+  scenario "#edit" do
+    FactoryGirl.create(:admin, id: 2, email: 'john@crowdint.com', name: 'Jonh Doe', is_admin: true)
+    FactoryGirl.create(:salary, id: 2, employee_id: 2, amount: 0.0)
+    FactoryGirl.create(:saving_company, id: 2, employee_id: 2, amount: 0.0)
+    FactoryGirl.create(:saving_employee, id: 2, employee_id: 2, amount: 0.0)
+    visit root_path
+    click_link("Sign in with your crowdint account")
+    visit('/employees/2/edit')
+    find('#employee_paypal_email').set('john@paypal.com')
+    find('#employee_salary_attributes_amount').set('2000.0')
+    find('#employee_saving_company_attributes_amount').set('3.1')
+    find('#employee_saving_employee_attributes_amount').set('2.9')
+    first('.form-update-employee__submit-btn').click
+    visit('/employees/2/edit')
+    expect(page).to have_selector("input[value='john@paypal.com']")
+    expect(page).to have_selector("input[value='2000.0']")
+    expect(page).to have_selector("input[value='3.1']")
+    expect(page).to have_selector("input[value='2.9']")
+  end
+end
+
--- spec/support/helpers/changestatus_helper.rb
+++
@@ -1,6 +0,0 @@
-  def login
-   FactoryGirl.create(:admin, email: 'john@crowdint.com', name: 'Jonh Doe', is_admin: true, id: 1)
-   visit root_path
-   click_link("Sign in with your crowdint account")
-   expect(page).to have_content "Signed in as Jonh Doe!"
-  end
